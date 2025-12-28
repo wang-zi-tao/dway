ace59a957a089581e0fa54795883aeca7cbc8e86
wang-zi-tao <2451355023@qq.com>
2025-12-28 23:07:42 +0800

    fix: 修复UiMesh重复渲染问题

--- dway-ui-framework/src/widgets/shape.rs
+++ dway-ui-framework/src/widgets/shape.rs
@@ -9,7 +9,8 @@ pub struct UiShape;
 pub type UiShapeMaterial = MeshMaterial2d<ColorMaterial>;
 
 pub fn after_process_shape(mut query: Query<(&mut Mesh2d, &mut UiMesh), Changed<Mesh2d>>) {
-    for (mesh2d, mut ui_mesh) in &mut query {
+    for (mut mesh2d, mut ui_mesh) in &mut query {
         ui_mesh.set_if_neq(UiMesh::from(mesh2d.0.clone()));
+        *mesh2d = Default::default();
     }
 }
